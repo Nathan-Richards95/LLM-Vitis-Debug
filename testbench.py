b8f5db1e2b90c21bf60f00f65eced9948d74a484
Nathan Richards <nathan.rich7@gmail.com>
2026-03-28 21:14:40 -0700

    add initial testing for local llm

--- testbench.py
+++ testbench.py
@@ -1,5 +1,34 @@
+from transformers import pipeline
 import constants
 
 if __name__ == '__main__':
     print("Hello!")
-    #1. 
+    #1. iterate through each test case in the test cases directory
+    #2. grab the broken code and send it to the LLM
+    pipe = pipeline(
+        task="text-generation",
+        model="openai/gpt-oss-20b",     # or whatever variant you installed
+        device_map="auto",              # automatically use GPU if available
+        dtype="auto",
+    )
+    message = constants.BASE_PROMPT
+    code = ""
+    messages = [
+        {
+            "role": "system",
+            "content": (constants.BASE_PROMPT),
+        },
+        {
+            "role": "user",
+            "content": f"===BEGIN BROKEN CODE===\n{code}\n===END BROKEN CODE===",  
+        }
+    ]
+    #3. grab the output from the LLM
+    out = pipe(messages, max_new_tokens=2048)
+    print(f'{out}')
+    fixed_code = out[0]["generated_text"][-1]["content"]
+    #4. check the following from the output:
+    #   - is the output empty
+    #   - is the response just code
+    #   - does the top level function still exist
+    #   - required includes still exist
